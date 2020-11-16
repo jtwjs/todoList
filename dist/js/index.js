6cde8f0d0312d56720febbb5b1d2f45f4f9c376e
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-11-16 16:08:18 +0900

    NPM: Babel install

---
+++ dist/js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var arr = [1, 2, 3];
+
+for (var _i = 0, _arr = arr; _i < _arr.length; _i++) {
+  var num = _arr[_i];
+  console.log(num);
+}
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+const arr = [1,2,3];
+
+for(const num of arr) {
+    console.log(num);
+}
