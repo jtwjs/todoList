a33ae738dc50a525886693ea9c17295cc06b4553
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-11-20 16:26:03 +0900

    JS: prepare.js

---
+++ src/js/prepare.js
@@ -0,0 +1,18 @@
+
+
+
+export const prepare = {
+        obj: {
+            form: document.querySelector('.prepare-form'),
+            inputName: document.querySelector('#input--name'),
+            createBtn: document.querySelector('.continue-btn'),
+        },
+        registerName(storageName,name) {
+            localStorage.setItem(storageName,name);
+        },
+    }
+    
+
+
+
+
