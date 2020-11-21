cbdf9b3a7858ecc087a7edd40970a9ac6088b2d0
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-11-21 13:23:22 +0900

    Modify: prepare -> question

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,32 @@
 'use strict';
 
-import {prepare} from "./prepare.js"
-
+// import {prepare} from "./prepare.js"
+(() => {
     const USER_LS = "currentUser";
-    console.log(prepare);
+    const sceneInfo = [{
+        objs: {
+            container: document.querySelector('.question'),
+            form: document.querySelector('.question-form'),
+            inputName: document.querySelector('#input--name'),
+            createBtn: document.querySelector('.continue-btn'),
+        },
+    }];
+
+
+
+
+    registerName(name) {
+        localStorage.setItem(USER_LS,name);
+    }
+
+    sceneInfo[0].objs.form.addEventListener('submit', () => {
+        if(sceneInfo.objs.inputName.value)
+            registerName(sceneInfo.objs.inputName.value);
+        
+    });
+    sceneInfo[0].objs.createBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        if(sceneInfo.objs.inputName.value)
+            registerName(sceneInfo.objs.inputName.value);
+    });
+})();
