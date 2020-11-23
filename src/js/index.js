ca0d727875c90798bcebc9408de445a7b2acfd52
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-11-23 18:45:09 +0900

    JS: todo list

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@
     const API_KEY = "83853936bbc3546d9a97b185b48b6449";
     const COORDS = "coords";
     const USER_LS = "currentUser";
+    const TODO_LS = "todo-list'";
+    const toDos = [];
     const sceneInfo = [{
         objs: {
             container: document.querySelector('.question'),
@@ -28,6 +30,7 @@
             local: document.querySelector('.local__name'),
             clock: document.querySelector('.current-time'),
             greeting: document.querySelector('.greeting-by-time'),
+            inputTodo: document.querySelector('#input--todo'),
         }
                         
                     }];
@@ -64,8 +67,8 @@
                 if(this.value){
                     registerName(this.value);
                     this.nextElementSibling.classList.remove('active');
-                    greeting();
                     nextQuestion();
+                    greeting();
                     }
                 else {
                     this.nextElementSibling.classList.add('active');
@@ -91,12 +94,10 @@
                 return response.json();
             })
             .then(function (json) {
-                console.log(json);
                 const icon = json.weather[0].icon;
                 const temperature = Math.round(json.main.temp);
                 const place = json.name;
                 const index = place.indexOf("-");
-                console.log(sceneInfo[1].objs.weatherIcon.style.background)
                 sceneInfo[1].objs.weatherIcon.style.backgroundImage = `url(http://openweathermap.org/img/wn/${icon}@2x.png)`;
                 sceneInfo[1].objs.temperature.innerText = temperature;
                 sceneInfo[1].objs.local.innerText = place.slice(0,index);
@@ -161,8 +162,40 @@
         function greetingByTime(str) {
             sceneInfo[1].objs.greeting.innerText = 
                 `Good ${str}, ${sceneInfo[0].values.userInfo.name}`;
+                console.log(sceneInfo[0].values.userInfo.name)
         }
     }
+
+    /*Todo */
+    function saveToDos() {
+        localStorage.setItem(TODO_LS, JSON.stringify(toDos));
+    }
+
+    function paintToDo(text) {
+        const newId = toDos.length + 1;
+        const toDoObj = {
+            id: newId,
+            text: text,
+        }
+        toDos.push(toDoObj);
+        saveToDos();
+    }
+
+    function todoSubmit(){
+        const value = this.value;
+        paintToDo(value);
+        this.value = '';
+    }
+    
+    function loadToDos() {
+        const loadedToDos = localStorage.getItem(TODO_LS);
+        if(loadedToDos !== null) {
+            const parseToDos = JSON.parse(loadedToDos);
+            for (const obj of parseToDos) {
+                paintToDo(obj.text);
+            }
+        }
+    };
 
     sceneInfo[0].objs.inputQuestion.addEventListener('keydown', function(event){
         if(event.keyCode === 13) {
@@ -179,17 +212,25 @@
     sceneInfo[0].objs.container.addEventListener('transitionend', (e) => {
         if(sceneInfo[0].values.userInfo.age)
         document.body.removeChild(e.currentTarget);
-    })
+    });
+
+    sceneInfo[1].objs.inputTodo.addEventListener('keydown', function(event) {
+        if(event.keyCode === 13) {
+            todoSubmit.call(this);
+        }
+    });
 
     window.addEventListener('DOMContentLoaded', () => {
-        const userInfo = JSON.parse(localStorage.getItem(USER_LS));
+        const loadedUserInfo = localStorage.getItem(USER_LS);
         getCurrentTime();      
         setInterval(getCurrentTime, 1000);
         loadCoords();
-        greeting();
-        if(userInfo) {
-            sceneInfo[0].values.userInfo = userInfo;
-            if(!userInfo.age){
+        loadToDos();
+        if(loadedUserInfo !== null) {
+            const parseUserInfo = JSON.parse(loadedUserInfo);
+            sceneInfo[0].values.userInfo = parseUserInfo;
+            greeting();
+            if(!parseUserInfo.age){
                 ageQuestion();
                 return;
             }
